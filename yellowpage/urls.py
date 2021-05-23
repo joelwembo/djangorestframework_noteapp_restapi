49227f86349c3afdd96140cbbc73f4729335bf88
Joel Wembo <joelotepawembo@gmail.com>
2021-05-23 15:57:05 +0800

    adding cookies and sessions

--- yellowpage/urls.py
+++ yellowpage/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
+
+from django.views.generic import TemplateView
 from . import views
 
 # direct to view home conents
 urlpatterns = [
     path('', views.home, name='yellowpage-home'),
+    path('connection/', views.login, name='yellowpage-login'),
+
   
 ]
--- yellowpage/views.py
+++ yellowpage/views.py
@@ -1,8 +1,28 @@
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import LoginForm
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Blog Home</h1>')
    
     return render(request, 'yellowpage/home.html')
+
+def login(request):
+    username = "not logged in"
+
+    return render(request, 'yellowpage/login.html')
+   
+    if request.method == "POST":
+          #Get the posted form
+      MyLoginForm = LoginForm(request.POST)
+      
+      if MyLoginForm.is_valid():
+         username = MyLoginForm.cleaned_data['username']
+    else:
+        MyLoginForm = LoginForm()
+		
+    return render(request, 'yellowpage/loggedin.html', {"username" : username})    
